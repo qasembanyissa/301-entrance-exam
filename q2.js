01c317d1fc36cc103317e7c2578f25d83515a199
qasembanyissa <q0790405801@gmail.com>
2020-03-16 13:42:39 +0200

    q2 added

---
+++ q2.js
@@ -0,0 +1,12 @@
+'use strict'
+var str = '*';
+ for (var i=0;i<6;i++){
+     for (var j = 0 ; j < i ; j++){
+        //  console.log(str);
+            document.write(str);
+     }
+  document.write('<br />');
+// console.log('<br />');
+ 
+     
+ }
